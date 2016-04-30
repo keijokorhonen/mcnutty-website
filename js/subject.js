16d3f39fbadb3ebf60b058a832ff09fdd15e6fea
Devin McNulty <mcnultydp@gmail.com>
2016-04-30 17:02:16 +0200

    Physics page all one page
    - combined the physics and assessment pages so that content not loaded
    twice
    - added filler syllabus page

--- js/subject.js
+++ js/subject.js
@@ -7,7 +7,23 @@ var main = function() { // js running on page
     $(this).addClass('title-menu-item-selected'); // add menu item selected class
     $(this).children('a').addClass('title-menu-link-active');
   });
+  $('#menu-assessments').click(function() { // change content on menu item click
+    $('#chapters').hide();
+    $('#syllabus').hide();
+    $('#assessments').show();
+  });
+  $('#menu-chapters').click(function() { // change content on menu item click
+    $('#assessments').hide();
+    $('#syllabus').hide();
+    $('#chapters').show();
+  });
+  $('#menu-syllabus').click(function() { // change content on menu item click
+    $('#assessments').hide();
+    $('#chapters').hide();
+    $('#syllabus').show();
+  });
 };
+
 
 
 $(document).ready(main); // loads the jquery
